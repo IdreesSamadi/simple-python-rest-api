a0e7d5b89949d010c78286faab51bd1d59f025b4
Idrees <57256007+IdreesSamadi@users.noreply.github.com>
2021-03-01 16:39:42 +0100

    added user profile feed

--- python_api/admin.py
+++ python_api/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from python_api import models
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
 
 # Register your models here.
