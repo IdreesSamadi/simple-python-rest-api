af07ddd431f75f630023303b681a8fb8dc8049fd
Idrees <57256007+IdreesSamadi@users.noreply.github.com>
2021-03-01 14:30:23 +0100

    login Api

--- python_api/views.py
+++ python_api/views.py
@@ -7,7 +7,8 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from python_api import permissions
 from rest_framework import filters
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 class HelloApiView(APIView):
   """Test Api view"""
@@ -93,3 +94,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
   permission_classes = (permissions.UpdateOwnProfile,)
   filter_backends = (filters.SearchFilter,)
   search_fields = ('name','email')
+
+
+class UserLoginApiView(ObtainAuthToken):
+  """handle create user authentication token"""
+  renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
